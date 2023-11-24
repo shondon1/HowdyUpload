e5467a16642fde3b983f344af2aab8c86e49a966
Rashon <rashyslop@outlook.com>
2023-11-24 12:07:32 -0700

    Initial Commit

---
+++ HowdyUpload/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HowdyUpload
+//
+//  Created by Rashon Hyslop on 11/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HowdyUpload/HowdyUploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowdyUploadApp.swift
+//  HowdyUpload
+//
+//  Created by Rashon Hyslop on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct HowdyUploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
