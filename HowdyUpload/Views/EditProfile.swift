1fc7d1e23c6771dab742689ee8b4d76f777a6954
Rashon <rashyslop@outlook.com>
2024-01-26 21:53:56 -0700

    Updated the Contentview

--- HowdyUpload/Views/EditProfile.swift
+++ HowdyUpload/Views/EditProfile.swift
@@ -1,18 +1,18 @@
 //
-//  CommentView.swift
+//  EditProfile.swift
 //  HowdyUpload
 //
-//  Created by Rashon Hyslop on 11/27/23.
+//  Created by Rashon Hyslop on 1/26/24.
 //
 
 import SwiftUI
 
-struct CommentView: View {
+struct EditProfile: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    CommentView()
+    EditProfile()
 }
